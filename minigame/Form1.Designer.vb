8d253d9ed04baf84404005fbab0d626d3bd91c6b
RYOTEA <hibbanfadluu@gmail.com>
2025-03-06 10:35:48 +0700

    Add project files.

---
+++ minigame/Form1.Designer.vb
@@ -0,0 +1,212 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.txt1 = New System.Windows.Forms.TextBox()
+        Me.btnSAVE = New System.Windows.Forms.Button()
+        Me.btnZERO = New System.Windows.Forms.Button()
+        Me.btnHapus = New System.Windows.Forms.Button()
+        Me.btncek = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btn1
+        '
+        Me.btn1.Location = New System.Drawing.Point(12, 73)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(100, 100)
+        Me.btn1.TabIndex = 0
+        Me.btn1.Text = "1"
+        Me.btn1.UseVisualStyleBackColor = True
+        '
+        'btn2
+        '
+        Me.btn2.Location = New System.Drawing.Point(118, 73)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(100, 100)
+        Me.btn2.TabIndex = 1
+        Me.btn2.Text = "2"
+        Me.btn2.UseVisualStyleBackColor = True
+        '
+        'btn3
+        '
+        Me.btn3.Location = New System.Drawing.Point(227, 73)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(100, 100)
+        Me.btn3.TabIndex = 2
+        Me.btn3.Text = "3"
+        Me.btn3.UseVisualStyleBackColor = True
+        '
+        'btn6
+        '
+        Me.btn6.Location = New System.Drawing.Point(227, 179)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(100, 100)
+        Me.btn6.TabIndex = 5
+        Me.btn6.Text = "6"
+        Me.btn6.UseVisualStyleBackColor = True
+        '
+        'btn5
+        '
+        Me.btn5.Location = New System.Drawing.Point(121, 179)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(100, 100)
+        Me.btn5.TabIndex = 4
+        Me.btn5.Text = "5"
+        Me.btn5.UseVisualStyleBackColor = True
+        '
+        'btn4
+        '
+        Me.btn4.Location = New System.Drawing.Point(12, 179)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(100, 100)
+        Me.btn4.TabIndex = 3
+        Me.btn4.Text = "4"
+        Me.btn4.UseVisualStyleBackColor = True
+        '
+        'btn9
+        '
+        Me.btn9.Location = New System.Drawing.Point(227, 285)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(100, 100)
+        Me.btn9.TabIndex = 8
+        Me.btn9.Text = "9"
+        Me.btn9.UseVisualStyleBackColor = True
+        '
+        'btn8
+        '
+        Me.btn8.Location = New System.Drawing.Point(121, 285)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(100, 100)
+        Me.btn8.TabIndex = 7
+        Me.btn8.Text = "8"
+        Me.btn8.UseVisualStyleBackColor = True
+        '
+        'btn7
+        '
+        Me.btn7.Location = New System.Drawing.Point(12, 284)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(100, 100)
+        Me.btn7.TabIndex = 6
+        Me.btn7.Text = "7"
+        Me.btn7.UseVisualStyleBackColor = True
+        '
+        'txt1
+        '
+        Me.txt1.Location = New System.Drawing.Point(12, 4)
+        Me.txt1.Multiline = True
+        Me.txt1.Name = "txt1"
+        Me.txt1.Size = New System.Drawing.Size(315, 63)
+        Me.txt1.TabIndex = 9
+        Me.txt1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'btnSAVE
+        '
+        Me.btnSAVE.Location = New System.Drawing.Point(224, 391)
+        Me.btnSAVE.Name = "btnSAVE"
+        Me.btnSAVE.Size = New System.Drawing.Size(100, 100)
+        Me.btnSAVE.TabIndex = 12
+        Me.btnSAVE.Text = "SAVE"
+        Me.btnSAVE.UseVisualStyleBackColor = True
+        '
+        'btnZERO
+        '
+        Me.btnZERO.Location = New System.Drawing.Point(118, 391)
+        Me.btnZERO.Name = "btnZERO"
+        Me.btnZERO.Size = New System.Drawing.Size(100, 100)
+        Me.btnZERO.TabIndex = 11
+        Me.btnZERO.Text = "0"
+        Me.btnZERO.UseVisualStyleBackColor = True
+        '
+        'btnHapus
+        '
+        Me.btnHapus.Location = New System.Drawing.Point(9, 390)
+        Me.btnHapus.Name = "btnHapus"
+        Me.btnHapus.Size = New System.Drawing.Size(100, 100)
+        Me.btnHapus.TabIndex = 10
+        Me.btnHapus.Text = "X"
+        Me.btnHapus.UseVisualStyleBackColor = True
+        '
+        'btncek
+        '
+        Me.btncek.Location = New System.Drawing.Point(224, 391)
+        Me.btncek.Name = "btncek"
+        Me.btncek.Size = New System.Drawing.Size(100, 99)
+        Me.btncek.TabIndex = 13
+        Me.btncek.Text = "check"
+        Me.btncek.UseVisualStyleBackColor = True
+        Me.btncek.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(330, 495)
+        Me.Controls.Add(Me.btncek)
+        Me.Controls.Add(Me.btnSAVE)
+        Me.Controls.Add(Me.btnZERO)
+        Me.Controls.Add(Me.btnHapus)
+        Me.Controls.Add(Me.txt1)
+        Me.Controls.Add(Me.btn9)
+        Me.Controls.Add(Me.btn8)
+        Me.Controls.Add(Me.btn7)
+        Me.Controls.Add(Me.btn6)
+        Me.Controls.Add(Me.btn5)
+        Me.Controls.Add(Me.btn4)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.btn1)
+        Me.KeyPreview = True
+        Me.MaximumSize = New System.Drawing.Size(346, 534)
+        Me.MinimumSize = New System.Drawing.Size(346, 534)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents txt1 As TextBox
+    Friend WithEvents btnSAVE As Button
+    Friend WithEvents btnZERO As Button
+    Friend WithEvents btnHapus As Button
+    Friend WithEvents btncek As Button
+End Class
---
+++ minigame/Form1.vb
@@ -0,0 +1,70 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        txt1.Text &= btn1.Text
+    End Sub
+
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        txt1.Text &= btn2.Text
+    End Sub
+
+    Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        txt1.Text &= btn3.Text
+    End Sub
+
+    Private Sub btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+        txt1.Text &= btn4.Text
+    End Sub
+
+    Private Sub btn5_Click(sender As Object, e As EventArgs) Handles btn5.Click
+        txt1.Text &= btn5.Text
+    End Sub
+
+    Private Sub btn6_Click(sender As Object, e As EventArgs) Handles btn6.Click
+        txt1.Text &= btn6.Text
+    End Sub
+
+    Private Sub btn7_Click(sender As Object, e As EventArgs) Handles btn7.Click
+        txt1.Text &= btn7.Text
+    End Sub
+
+    Private Sub btn8_Click(sender As Object, e As EventArgs) Handles btn8.Click
+        txt1.Text &= btn8.Text
+    End Sub
+
+    Private Sub btn9_Click(sender As Object, e As EventArgs) Handles btn9.Click
+        txt1.Text &= btn9.Text
+    End Sub
+
+    Private Sub btnZERO_Click(sender As Object, e As EventArgs) Handles btnZERO.Click
+        txt1.Text &= btnZERO.Text
+    End Sub
+
+    Private Sub btnHapus_Click(sender As Object, e As EventArgs) Handles btnHapus.Click
+        If txt1.Text.Length > 0 Then
+            txt1.Text = txt1.Text.Remove(txt1.Text.Length - 1)
+        End If
+
+    End Sub
+    Dim pin As Integer
+    Private Sub SavePin(sender As Object, e As EventArgs) Handles btnSAVE.Click
+        pin = txt1.Text
+        MessageBox.Show("TERSIMPAN")
+        txt1.Text = ""
+        Console.WriteLine(pin)
+        btncek.Visible = True
+        btnSAVE.Visible = False
+    End Sub
+
+    Private Sub btncek_Click(sender As Object, e As EventArgs) Handles btncek.Click
+        If txt1.Text = pin Then
+            MessageBox.Show("Pin Benar")
+        Else
+            MessageBox.Show("Pin Salah")
+        End If
+        'messagebox digunakan untuk memuncul windows baru untuk menampilkan pesan
+    End Sub
+End Class
